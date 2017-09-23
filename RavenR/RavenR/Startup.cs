211fa193c0271d2299a54bc45d6eb5fd693c4310
Armand Jordaan <armandjordaan@live.co.za>
2017-09-23 23:58:04 +0200

    inital commit

---
+++ RavenR/RavenR/Startup.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.SpaServices.Webpack;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace RavenR
+{
+  public class Startup
+  {
+    public Startup(IConfiguration configuration)
+    {
+      Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddMvc();
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+        app.UseBrowserLink();
+
+        // Setup WebpackDevMidleware for 
+        //"Hot module replacement" while debugging
+        app.UseWebpackDevMiddleware(new WebpackDevMiddlewareOptions
+        { HotModuleReplacement = true });
+      }
+      else
+      {
+        app.UseExceptionHandler("/Home/Error");
+      }
+
+      app.UseStaticFiles();
+
+      app.UseMvc(routes =>
+      {
+        routes.MapRoute(
+                  name: "default",
+                  template: "{controller=Home}/{action=Index}/{id?}");
+
+
+        // Setup additional routing for SPA
+        routes.MapSpaFallbackRoute(
+        "spa-fallback",
+        new { controller = "Home", action = "Index" });
+      });
+    }
+  }
+}
